289d48e5e29a32a8a1558ece773a8ade3518a31a
Jorji49 <kamaahmetkayra303@outlook.com>
2026-08-29 16:10:45 +0300

    chore: homebrew v1.1.0 tarball hash

--- Formula/streamtop.rb
+++ Formula/streamtop.rb
@@ -4,7 +4,7 @@ class Streamtop < Formula
   desc "HLS/DASH/IPTV stream diagnostics in the terminal"
   homepage "https://github.com/Jorji49/streamtop"
   url "https://github.com/Jorji49/streamtop/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "177facf436a7be07b8b35cad04dd0bac137aad1991877462902ee2cf99c6abe3"
+  sha256 "23ff59ee037095112e5d4c87a1ae219a5304f3892e3054bb541fa1016453e02f"
   license "MIT"
   head "https://github.com/Jorji49/streamtop.git", branch: "main"
 
